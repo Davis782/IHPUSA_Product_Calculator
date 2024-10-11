fa7543b3ff813f3edc35a61eeeca3066d04876b2
Davis782 <67481353+Davis782@users.noreply.github.com>
2024-10-11 12:24:40 -0400

    Update Taipy_Cost_per_Ounce_Profit.py
    Update Taipy_Cost_per_Ounce_Profit

--- Taipy_Cost_per_Ounce_Profit.py
+++ Taipy_Cost_per_Ounce_Profit.py
@@ -1,7 +1,7 @@
 import taipy as tp
 from taipy import Gui
 
-# Define the input values
+# Define the input values as a state variable
 input_values = {
     'cost_product1': 0,
     'cost_product2': 0,
@@ -16,37 +16,30 @@ input_values = {
     'cbd_choice': '1'
 }
 
-# Define the Taipy GUI
-gui = Gui(page="# Cost Calculator")
+# Define the Taipy GUI layout using Markdown-like syntax
+gui = Gui(page="""
+# Cost Calculator
 
-# Define the input fields
-gui.add_input_field("cost_product1", "Cost of Product 1",
-                    input_values['cost_product1'])
-gui.add_input_field("cost_product2", "Cost of Product 2",
-                    input_values['cost_product2'])
-gui.add_input_field("cost_product3", "Cost of Product 3",
-                    input_values['cost_product3'])
-gui.add_input_field("labor_cost_per_hour", "Labor Cost per Hour",
-                    input_values['labor_cost_per_hour'])
-gui.add_input_field("cost_cbd_per_ounce", "Cost of CBD per Ounce",
-                    input_values['cost_cbd_per_ounce'])
-gui.add_input_field("wholesale_markup", "Wholesale Markup (%)",
-                    input_values['wholesale_markup'])
-gui.add_input_field("retail_markup", "Retail Markup (%)",
-                    input_values['retail_markup'])
-gui.add_input_field("total_ounces", "Total Ounces",
-                    input_values['total_ounces'])
-gui.add_input_field("ounces_per_bottle", "Ounces per Bottle",
-                    input_values['ounces_per_bottle'])
-gui.add_input_field("bottles", "Bottles", input_values['bottles'])
-gui.add_select("cbd_choice", "CBD Options", [
-               "1", "2"], input_values['cbd_choice'])
+## Input Values
+* Cost of Product 1: <|cost_product1|>
+* Cost of Product 2: <|cost_product2|>
+* Cost of Product 3: <|cost_product3|>
+* Labor Cost per Hour: <|labor_cost_per_hour|>
+* Cost of CBD per Ounce: <|cost_cbd_per_ounce|>
+* Wholesale Markup (%): <|wholesale_markup|>
+* Retail Markup (%): <|retail_markup|>
+* Total Ounces: <|total_ounces|>
+* Ounces per Bottle: <|ounces_per_bottle|>
+* Bottles: <|bottles|>
+* CBD Options: <|cbd_choice|>
 
-# Define the calculate button
+## Actions
+<|Calculate|>
+""")
 
-
+# Define the calculate function
 def calculate(state):
-    # Get the input values
+    # Get the input values from the state
     input_values['cost_product1'] = state.cost_product1
     input_values['cost_product2'] = state.cost_product2
     input_values['cost_product3'] = state.cost_product3
@@ -75,12 +68,9 @@ def calculate(state):
         result, input_values['wholesale_markup'], input_values['retail_markup'], input_values['total_ounces'])
 
     # Calculate the profit per ounce
-    distributor_profit_per_ounce = profit['distributor_profit'] / \
-        input_values['total_ounces']
-    manufacturer_profit_per_ounce = profit['manufacturer_profit'] / \
-        input_values['total_ounces']
-    retailer_profit_per_ounce = profit['retailer_profit'] / \
-        input_values['total_ounces']
+    distributor_profit_per_ounce = profit['distributor_profit'] / input_values['total_ounces']
+    manufacturer_profit_per_ounce = profit['manufacturer_profit'] / input_values['total_ounces']
+    retailer_profit_per_ounce = profit['retailer_profit'] / input_values['total_ounces']
 
     # Return the results
     return {
@@ -99,9 +89,24 @@ def calculate(state):
         'retailer_profit_per_ounce': retailer_profit_per_ounce
     }
 
-
+# Add the calculate function to the GUI
 gui.add_button("Calculate", calculate)
 
 # Define the output fields
 gui.add_output_field("total_cost", "Total Cost")
-gui.add_output
+gui.add_output_field("cost_per_ounce_manufacturing", "Cost per Ounce Manufacturing")
+gui.add_output_field("cost_per_ounce_distribution", "Cost per Ounce Distribution")
+gui.add_output_field("total_cost_per_ounce", "Total Cost per Ounce")
+gui.add_output_field("wholesale_price", "Wholesale Price")
+gui.add_output_field("retail_price", "Retail Price")
+gui.add_output_field("total_profit", "Total Profit")
+gui.add_output_field("distributor_profit", "Distributor Profit")
+gui.add_output_field("manufacturer_profit", "Manufacturer Profit")
+gui.add_output_field("retailer_profit", "Retailer Profit")
+gui.add_output_field("distributor_profit_per_ounce", "Distributor Profit per Ounce")
+gui.add_output_field("manufacturer_profit_per_ounce", "Manufacturer Profit per Ounce")
+gui.add_output_field("retailer_profit_per_ounce", "Retailer Profit per Ounce")
+
+# Run the Taipy GUI
+if __name__ == "__main__":
+    gui.run()
